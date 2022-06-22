d8e5e15705336145b75a2b6661ca00c1fb09a0b0
Frédéric Morel <frederic.morel.ars@outlook.fr>
2022-06-22 16:12:47 +0400

    maj exo1

--- partie 1/App.js
+++ partie 1/App.js
@@ -4,13 +4,13 @@ const person = [
   { nom: "Benjamin", age: 10, sexe: "m" },
   { nom: "Chen Li", age: 28, sexe: "f" },
 ];
-console.log(person[0].nom);
+
 /* Étape 2 affiche les infos sous forme de liste en utilisant ul présent dans html du projet  */
 const getList = document.getElementById("list_exo");
 for (const element of person) {
   const lineOfList = document.createElement("li");
   lineOfList.append("nom : ", element.nom);
-  lineOfList.append(", age : ", element.age, "ans, ");
+  lineOfList.append(", age : ", element.age, " ans, ");
   lineOfList.append("sexe : ", element.sexe === "m" ? "Homme" : "Femme");
   getList.appendChild(lineOfList);
 }
@@ -24,6 +24,41 @@ inputSearch.placeholder = "Tapez le nom recherché !";
 const mainElement = document.getElementById("app");
 mainElement.appendChild(inputSearch);
 
-inputSearch.addEventListener();
+inputSearch.addEventListener("keyup", (e) => {
+  e.preventDefault;
 
+  const resultat = person.filter((obj) => {
+    return obj.nom === e.target.value;
+  });
+  for (const el of resultat) {
+    console.log("résultat2 = ", el.nom);
+    const spanResult = document.createElement("div");
+    spanResult.className = "span-result";
+    spanResult.innerHTML = `${el.nom} a ${
+      el.age
+    } ans, il est de sexe Masculin ${(el.sexe = "m" ? "Masculin" : "Féminin")}`;
+    mainElement.appendChild(spanResult);
+  }
+
+  console.log("resultat : ", result);
+  const find = searchName(result);
+  console.log("j'ai trouvé : ", find);
+});
+console.log("la valeur de e target value : ", result);
 /* Étape 4 utiliser la propriété du type array qui permet de recherche avec un filter les éléments qui correspond a ce que utilisateur va entrer dans l'input de recherche   */
+let renvoye;
+function searchName(nameSearched) {
+  console.log("Nom recherché : ", nameSearched);
+  console.log(person);
+  person.filter(function (nameParam) {
+    console.log("ecoute : ", nameParam.nom, nameSearched);
+    if (nameParam.nom === nameSearched) {
+      renvoye = nameParam.nom;
+      return renvoye;
+    }
+    console.log("renvoyé et et : ", renvoye);
+    return renvoye;
+  });
+  console.log("avant renvoi : ", renvoye);
+  return renvoye;
+}
--- partie 2/App.js
+++ partie 2/App.js
@@ -1,30 +1,72 @@
 /* Etape 1 crée tableaux  vide en html qui doit avoir dans sont thead nom ,prénom ,classe ,professeur ,moyenne */
 
-
+// TODO : OK
 
 /* Etape 2   crée tableau d'objet qui va contenir les des info avec la structure suivante  {nom:string,prénom:string , classe:string ,professeur:string,moyenne:number} le array doit avoir 5 objet minimum*/
 
-
-
-
+students = [
+  {
+    name: "Tenrien",
+    firstName: "Jean",
+    class: "Terminal S1",
+    professor: "Ivan le tendre",
+    average: 19,
+  },
+  {
+    name: "Jete",
+    firstName: "pascal",
+    class: "Terminal S1",
+    professor: "Tony Stark",
+    average: 18,
+  },
+  {
+    name: "Muren",
+    firstName: "Pierre",
+    class: "Terminal S1",
+    professor: "Ichiro",
+    average: 17,
+  },
+  {
+    name: "Ouaisquiz",
+    firstName: "Yvon",
+    class: "Terminal S1",
+    professor: "jack Daniel's",
+    average: 16,
+  },
+  {
+    name: "Sense",
+    firstName: "René",
+    class: "Terminal S1",
+    professor: "Jonhy walker",
+    average: 15,
+  },
+];
 
 /* Etape 3  afficher les dans le tableaux Html avec du JS  */
+const tBody = document.querySelector("tbody");
 
-
-
-
-
+students.forEach((student) => {
+  const rowToAdd = document.createElement("tr");
+  rowToAdd.className = "student-row";
+  rowToAdd.innerHTML = `
+                    <td>${student.name}</td>
+                    <td>${student.firstName}</td>
+                    <td>${student.class}</td>
+                    <td>${student.professor}</td>
+                    <td>${student.average}</td>
+                    `;
+  tBody.appendChild(rowToAdd);
+});
 
 /* Etape 4 Ajouter un champ de recherche au-dessus du tableaux  */
 
+/* Etape 5 récupérer les valeurs entrées dans le champ de recherche */
+const searchField = document.querySelector("#student");
+searchField.addEventListener("blur", (e) => {
+  e.preventDefault();
+  let contentRow = document.querySelector(".student-row");
+  console.log("avant le while", contentRow);
 
-
-
-/* Etape 5 récupérait les valeur entrée dans le champ de recherche */
-
-
-
-
-
+  console.log("first line", contentRow.firstChild);
+});
 /* Etape 6  quand on tape le nom d'un élevé ou d'un professeur faire une recherche dans le tableau */
-
